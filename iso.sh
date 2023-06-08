1d4e25a1be3e889db7048c69b758182da112e827
Ryan Wolf <ryantw2@illinois.edu>
2023-06-07 19:15:45 -0500

    Rename to FreeTime

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/freetime.kernel isodir/boot/freetime.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "freetime" {
+	multiboot /boot/freetime.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o freetime.iso isodir
